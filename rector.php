fd557e3bf2e24f1285961248872e5438359321db
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-31 13:52:36 +0100

    refactor: update rector configuration for improved code quality and sets
    - Replaced the previous configuration method with a new fluent interface.
    - Updated paths to include additional directories: app/src, factories, and fixtures.
    - Set PHP version to 8.2 and included various quality sets.
    - Removed unused imports automatically.
    - Added type coverage level and specific rules for better code quality.

--- rector.php
+++ rector.php
@@ -1,47 +1,51 @@
 <?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 31/01/2025, 13:52
+ *
+ * @project IDMarinas Maker Bundle
+ * @see     https://github.com/idmarinas/maker-bundle
+ *
+ * @file    rector.php
+ * @date    31/01/2025
+ * @time    13:52
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   2.0.0
+ */
 
 declare(strict_types=1);
 
-use Rector\Set\ValueObject\SetList;
-use Rector\Symfony\Set\TwigSetList;
-use Rector\Core\Configuration\Option;
+use Rector\Config\RectorConfig;
 use Rector\Symfony\Set\SymfonySetList;
-use Rector\Core\ValueObject\PhpVersion;
-use Rector\Doctrine\Set\DoctrineSetList;
-use Rector\CodeQuality\Rector\Include_\AbsolutizeRequireAndIncludePathRector;
-use Rector\CodeQuality\Rector\Array_\CallableThisArrayToAnonymousFunctionRector;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 
-return static function (ContainerConfigurator $containerConfigurator): void
-{
-    // get parameters
-    $parameters = $containerConfigurator->parameters();
-
-    $parameters->set(Option::PATHS, [
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-    ]);
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_74);
-    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
-    $parameters->set(Option::IMPORT_DOC_BLOCKS, false);
-
-    $containerConfigurator->import(SetList::DEAD_CODE);
-    $containerConfigurator->import(SetList::CODE_QUALITY);
-    $containerConfigurator->import(SetList::PHP_74);
-    $containerConfigurator->import(SetList::PHP_80);
-    $containerConfigurator->import(SetList::PHP_81);
-
-    //-- Symfony Framework
-    $containerConfigurator->import(SymfonySetList::SYMFONY_44);
-    $containerConfigurator->import(SymfonySetList::SYMFONY_50);
-    $containerConfigurator->import(TwigSetList::TWIG_240);
-    $containerConfigurator->import(DoctrineSetList::DOCTRINE_DBAL_210);
-    $containerConfigurator->import(DoctrineSetList::DOCTRINE_DBAL_211);
-    $containerConfigurator->import(DoctrineSetList::DOCTRINE_DBAL_30);
-
-    //-- Skip some rules/files ...
-    $parameters->set(Option::SKIP, [
-        CallableThisArrayToAnonymousFunctionRector::class,
-        AbsolutizeRequireAndIncludePathRector::class
-    ]);
-};
+return RectorConfig::configure()
+	->withPaths([
+		__DIR__ . '/app/src',
+		__DIR__ . '/factories',
+		__DIR__ . '/fixtures',
+		__DIR__ . '/src',
+		__DIR__ . '/tests',
+	])
+	->withPhpSets(php82: true)
+	->withPreparedSets(
+		deadCode           : true,
+		codeQuality        : true,
+		codingStyle        : true,
+		doctrineCodeQuality: true,
+		symfonyCodeQuality : true,
+		symfonyConfigs     : true,
+		twig               : true
+	)
+	->withImportNames(removeUnusedImports: true)
+	->withTypeCoverageLevel(0)
+	->withSets([
+		SymfonySetList::SYMFONY_64,
+		SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
+	])
+	->withRules([AddVoidReturnTypeWhereNoReturnRector::class])
+;
