713310b3d0f728a30c55df85e5f581d2c669097e
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-11 00:05:55 +0100

    style(controller): format code in UserCrudController template for consistency

--- templates/user/bundle/src/Controller/Admin/User/UserCrudController.tpl.php
+++ templates/user/bundle/src/Controller/Admin/User/UserCrudController.tpl.php
@@ -19,58 +19,58 @@ use function Symfony\Component\Translation\t;
 
 final class UserCrudController extends AbstractCrudController
 {
-public static function getEntityFqcn (): string
-{
-return User::class;
-}
+	public static function getEntityFqcn (): string
+	{
+		return User::class;
+	}
 
-public function configureCrud (Crud $crud): Crud
-{
-return $crud
-->setSearchFields(['email', 'displayName'])
-->setSearchMode(SearchMode::ANY_TERMS)
-->setEntityLabelInPlural(t('entity.label.user.plural', [], 'IdmUserBundle'))
-->setEntityLabelInSingular(t('entity.label.user.singular', [], 'IdmUserBundle'))
-;
-}
+	public function configureCrud (Crud $crud): Crud
+	{
+		return $crud
+			->setSearchFields(['email', 'displayName'])
+			->setSearchMode(SearchMode::ANY_TERMS)
+			->setEntityLabelInPlural(t('entity.label.user.plural', [], 'IdmUserBundle'))
+			->setEntityLabelInSingular(t('entity.label.user.singular', [], 'IdmUserBundle'))
+		;
+	}
 
-public function configureFilters (Filters $filters): Filters
-{
-return $filters
-->add('email')
-->add('displayName')
-;
-}
+	public function configureFilters (Filters $filters): Filters
+	{
+		return $filters
+			->add('email')
+			->add('displayName')
+		;
+	}
 
-public function configureFields (string $pageName): iterable
-{
-// Tab Info
-yield FormField::addTab(t('form.tab.info', [], 'IdmUserBundle'), 'fa fa-info');
-yield FormField::addColumn(8);
-yield IdField::new('uuid', t('crud.common.uuid', [], 'IdmUserBundle'))->onlyOnDetail();
-yield TextField::new('displayName', t('crud.user.display_name', [], 'IdmUserBundle'));
-yield EmailField::new('email', t('crud.user.email', [], 'IdmUserBundle'))
-->setPermission('ROLE_SUPER_ADMIN') // Only a superuser can change this
-;
+	public function configureFields (string $pageName): iterable
+	{
+		// Tab Info
+		yield FormField::addTab(t('form.tab.info', [], 'IdmUserBundle'), 'fa fa-info');
+		yield FormField::addColumn(8);
+		yield IdField::new('uuid', t('crud.common.uuid', [], 'IdmUserBundle'))->onlyOnDetail();
+		yield TextField::new('displayName', t('crud.user.display_name', [], 'IdmUserBundle'));
+		yield EmailField::new('email', t('crud.user.email', [], 'IdmUserBundle'))
+			->setPermission('ROLE_SUPER_ADMIN') // Only a superuser can change this
+		;
 
-yield FormField::addColumn(4);
-yield BooleanField::new('isDeleted', t('crud.common.is_deleted', [], 'IdmUserBundle'))
-->hideOnForm()
-->renderAsSwitch(false)
-->setVirtual(true)
-;
-yield DateTimeField::new('deletedAt', t('crud.common.deleted_at', [], 'IdmUserBundle'))
-->hideOnIndex()
-->setPermission('ROLE_SUPER_ADMIN') // Only a superuser can change this
-;
+		yield FormField::addColumn(4);
+		yield BooleanField::new('isDeleted', t('crud.common.is_deleted', [], 'IdmUserBundle'))
+			->hideOnForm()
+			->renderAsSwitch(false)
+			->setVirtual(true)
+		;
+		yield DateTimeField::new('deletedAt', t('crud.common.deleted_at', [], 'IdmUserBundle'))
+			->hideOnIndex()
+			->setPermission('ROLE_SUPER_ADMIN') // Only a superuser can change this
+		;
 
-// Tab Roles
-yield FormField::addTab(t('form.tab.roles', [], 'IdmUserBundle'), 'fa-regular fa-id-badge')
-->setPermission('ROLE_SUPER_ADMIN');
-yield ChoiceField::new('roles', t('crud.common.roles', [], 'IdmUserBundle'))
-->hideOnIndex()
-->setTranslatableChoices(UserRolesEnum::toTranslatableChoices())
-->allowMultipleChoices()
-;
-}
+		// Tab Roles
+		yield FormField::addTab(t('form.tab.roles', [], 'IdmUserBundle'), 'fa-regular fa-id-badge')
+			->setPermission('ROLE_SUPER_ADMIN');
+		yield ChoiceField::new('roles', t('crud.common.roles', [], 'IdmUserBundle'))
+			->hideOnIndex()
+			->setTranslatableChoices(UserRolesEnum::toTranslatableChoices())
+			->allowMultipleChoices()
+		;
+	}
 }
