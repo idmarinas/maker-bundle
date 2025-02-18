9631181d0623eb632605963f31d37a11fafa65de
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-18 15:18:53 +0100

    feat(services): add service configuration for user bundle and common contact
    - Introduced a new `services.php` file for service configuration.
    - Registered `idm_maker.make.user_bundle_files` and
      `idm_maker.make.common_bundle.contact` services.
    - The user bundle service is configured with a dependency on the
      file manager service

---
+++ config/services.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright 2022-2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 11/02/2025, 16:11
+ *
+ * @project IDMarinas Maker Bundle
+ * @see     https://github.com/idmarinas/maker-bundle
+ *
+ * @file    services.php
+ * @date    23/01/2022
+ * @time    15:57
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use Idm\Bundle\Maker\Maker\Common\MakerCommonContact;
+use Idm\Bundle\Maker\Maker\User\MakerUserBundle;
+
+return function (ContainerConfigurator $container) {
+	// @formatter:off
+	$container->services()
+		->set('idm_maker.make.user_bundle_files', MakerUserBundle::class)
+			->args([
+				'$fileManager' => service('maker.file_manager')
+			])
+			->tag('maker.command')
+
+		->set('idm_maker.make.common_bundle.contact', MakerCommonContact::class)
+			->tag('maker.command')
+	;
+	// @formatter::on
+};
