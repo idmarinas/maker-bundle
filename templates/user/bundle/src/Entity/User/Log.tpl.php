19fa3f8e92b3ce7a4a896f4ee5919f84b8d01124
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-20 15:58:28 +0100

    fix(entity): update table and index names in User Log entity template
    - Changed table name from 'idm_user_log' to 'idm_user_user_log'.
    - Updated index names accordingly to reflect the new table name.

--- templates/user/bundle/src/Entity/User/Log.tpl.php
+++ templates/user/bundle/src/Entity/User/Log.tpl.php
@@ -8,12 +8,12 @@ use Gedmo\Loggable\Entity\MappedSuperclass\AbstractLogEntry;
 use Gedmo\Loggable\Entity\Repository\LogEntryRepository;
 
 #[ORM\Entity(repositoryClass: LogEntryRepository::class)]
-#[ORM\Table(name: 'idm_user_log', options: ['row_format' => 'DYNAMIC'])]
-#[ORM\Index(name: 'idm_user_log_log_class_lookup_idx', columns: ['object_class'])]
-#[ORM\Index(name: 'idm_user_log_log_date_lookup_idx', columns: ['logged_at'])]
-#[ORM\Index(name: 'idm_user_log_log_user_lookup_idx', columns: ['username'])]
-#[ORM\Index(name: 'idm_user_log_log_version_lookup_idx', columns: ['object_id', 'object_class', 'version'])]
+#[ORM\Table(name: 'idm_user_user_log', options: ['row_format' => 'DYNAMIC'])]
+#[ORM\Index(name: 'idm_user_user_log_log_class_lookup_idx', columns: ['object_class'])]
+#[ORM\Index(name: 'idm_user_user_log_log_date_lookup_idx', columns: ['logged_at'])]
+#[ORM\Index(name: 'idm_user_user_log_log_user_lookup_idx', columns: ['username'])]
+#[ORM\Index(name: 'idm_user_user_log_log_version_lookup_idx', columns: ['object_id', 'object_class', 'version'])]
 class Log extends AbstractLogEntry
 {
-/* All required columns are mapped through inherited superclass */
+	/* All required columns are mapped through inherited superclass */
 }
