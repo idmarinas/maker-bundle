25ad30b1f47942ac15868ab4780af7f9f1b8f4ed
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-20 16:00:44 +0100

    refactor(entity): rename Log class to UserLog for clarity

--- templates/user/bundle/src/Entity/User/UserLog.tpl.php
+++ templates/user/bundle/src/Entity/User/UserLog.tpl.php
@@ -13,7 +13,7 @@ use Gedmo\Loggable\Entity\Repository\LogEntryRepository;
 #[ORM\Index(name: 'idm_user_user_log_log_date_lookup_idx', columns: ['logged_at'])]
 #[ORM\Index(name: 'idm_user_user_log_log_user_lookup_idx', columns: ['username'])]
 #[ORM\Index(name: 'idm_user_user_log_log_version_lookup_idx', columns: ['object_id', 'object_class', 'version'])]
-class Log extends AbstractLogEntry
+class UserLog extends AbstractLogEntry
 {
 	/* All required columns are mapped through inherited superclass */
 }
