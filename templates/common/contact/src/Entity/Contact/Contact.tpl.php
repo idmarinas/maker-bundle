d07ffaaa6e4d34ed245b3d26f562f5b992373cd9
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-27 14:09:04 +0100

    feat(Contact): add constructor to initialize createdAt and updatedAt
    - Added a constructor to the Contact class to set createdAt and
      updatedAt properties using the DateTime class.

--- templates/common/contact/src/Entity/Contact/Contact.tpl.php
+++ templates/common/contact/src/Entity/Contact/Contact.tpl.php
@@ -3,6 +3,7 @@
 namespace <?= $namespace; ?>;
 
 <?= $use_statements; ?>
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Idm\Bundle\Common\Model\Entity\AbstractContact;
@@ -10,4 +11,11 @@ use Idm\Bundle\Common\Model\Entity\AbstractContact;
 #[ORM\Table(name: 'idm_common_contact')]
 #[ORM\Entity(repositoryClass: <?= $repository_class ?>::class)]
 #[Gedmo\Loggable(logEntryClass: <?= $log_entry_class ?>::class)]
-class Contact extends AbstractContact {}
+class Contact extends AbstractContact
+{
+	public function __construct ()
+	{
+		$this->createdAt = new DateTime();
+		$this->updatedAt = new DateTime();
+	}
+}
