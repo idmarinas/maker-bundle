c1622345d931e35e422936e95d8738262fef87c9
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-27 14:08:20 +0100

    refactor(contact): rename Log class to ContactLog for clarity

--- templates/common/contact/src/Entity/Contact/ContactLog.tpl.php
+++ templates/common/contact/src/Entity/Contact/ContactLog.tpl.php
@@ -13,7 +13,7 @@ use Gedmo\Loggable\Entity\Repository\LogEntryRepository;
 #[ORM\Index(name: 'idm_common_contact_log_date_lookup_idx', columns: ['logged_at'])]
 #[ORM\Index(name: 'idm_common_contact_log_user_lookup_idx', columns: ['username'])]
 #[ORM\Index(name: 'idm_common_contact_log_version_lookup_idx', columns: ['object_id', 'object_class', 'version'])]
-class Log extends AbstractLogEntry
+class ContactLog extends AbstractLogEntry
 {
 	/* All required columns are mapped through inherited superclass */
 }
