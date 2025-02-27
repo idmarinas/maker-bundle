355a5f780c8436800ad96cabe211478332a0744f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-27 14:06:03 +0100

    feat(user): add soft delete and unique constraints to User entity
    - Added SoftDeleteableEntity trait for soft deletion functionality.
    - Implemented UniqueEntity constraints for email and username fields
      to ensure uniqueness.

--- templates/user/bundle/src/Entity/User/User.tpl.php
+++ templates/user/bundle/src/Entity/User/User.tpl.php
@@ -5,15 +5,21 @@ namespace <?= $namespace; ?>;
 <?= $use_statements; ?>
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
 use Idm\Bundle\User\Model\Entity\AbstractUser;
 use Idm\Bundle\User\Traits\Entity\UserPremiumTrait;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: <?= $repository_class ?>::class)]
 #[ORM\Table(name: 'idm_user_user')]
+#[Gedmo\SoftDeleteable()]
 #[Gedmo\Loggable(logEntryClass: <?= $log_entry_class ?>::class)]
+#[UniqueEntity('email', message: 'idm_user_bundle.email.not_unique')]
+#[UniqueEntity('username', message: 'idm_user_bundle.username.not_unique')]
 class User extends AbstractUser
 {
 	use UserPremiumTrait;
+	use SoftDeleteableEntity;
 
 	public function __construct ()
 	{
