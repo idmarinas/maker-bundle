421dfea84452b47e471c552b2ddb05ebc59c7925
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-25 15:15:31 +0100

    feat(entity): add SettingUserLog entity template with ORM mappings

---
+++ templates/settings/bundle/src/Entity/Setting/SettingUserLog.tpl.php
@@ -0,0 +1,21 @@
+<?= "<?php\n" ?>
+
+namespace <?= $namespace; ?>;
+
+<?= $use_statements; ?>
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\Loggable\Entity\MappedSuperclass\AbstractLogEntry;
+use Gedmo\Loggable\Entity\Repository\LogEntryRepository;
+
+#[ORM\Entity(repositoryClass: LogEntryRepository::class)]
+#[ORM\Table(name: 'idm_settings_setting_user_log', options: ['row_format' => 'DYNAMIC'])]
+#[ORM\Index(name: 'idm_settings_setting_user_log_class_lookup_idx', columns: ['object_class'])]
+#[ORM\Index(name: 'idm_settings_setting_user_log_date_lookup_idx', columns: ['logged_at'])]
+#[ORM\Index(name: 'idm_settings_setting_user_log_user_lookup_idx', columns: ['username'])]
+#[ORM\Index(name: 'idm_settings_setting_user_log_version_lookup_idx', columns: ['object_id', 'object_class',
+'version'])]
+class SettingUserLog extends AbstractLogEntry
+{
+/* All required columns are mapped through inherited superclass */
+}
