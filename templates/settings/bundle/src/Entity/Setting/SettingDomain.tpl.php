bd580a4454803f7b242dc7cd8a61db83c87d1806
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-05-26 18:42:16 +0200

    fix: update unique entity field in SettingDomain to 'name' instead of 'domain'

--- templates/settings/bundle/src/Entity/Setting/SettingDomain.tpl.php
+++ templates/settings/bundle/src/Entity/Setting/SettingDomain.tpl.php
@@ -12,7 +12,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 /** Domain for settings of Symfony App */
 #[ORM\Table(name: 'idm_settings_setting_domain')]
 #[ORM\Entity(repositoryClass: <?= $repository_class ?>::class)]
-#[UniqueEntity(fields: 'domain', message: 'domain.not_unique')]
+#[UniqueEntity(fields: 'name', message: 'domain.not_unique')]
 #[ORM\EntityListeners([SettingDomainListener::class])]
 #[Gedmo\Loggable(logEntryClass: <?= $log_entry_class ?>::class)]
 class SettingDomain extends AbstractSettingDomain {}
